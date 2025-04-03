87c46339efca7be8c7636ef139ca1e4b91c93ee7
Higor Menzato <higormenzato16@gmail.com>
2025-04-03 14:53:08 -0300

    Bad solution

--- src/application/Program.java
+++ src/application/Program.java
@@ -27,7 +27,22 @@ public class Program {
 		else {
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation: " + reservation);
+			System.out.println();
+			System.out.println("Check-In date (dd/MM/yyyy): ");
+			checkIn = sdf.parse(sc.next());
+			System.out.println("Check-Out date (dd/MM/yyyy): ");
+			checkOut = sdf.parse(sc.next());
+
+			String error = reservation.updateDates(checkIn, checkOut);
+			if(error == null) {
+				System.out.println("Error in reservation: " + error);
+			}
+			else {
+				System.out.println("Reservation: " + reservation);
+			}
+			
 		}
+		
 		
 		sc.close();
 
